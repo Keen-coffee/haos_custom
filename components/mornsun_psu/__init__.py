f48dc09f7d057334aa741541080563c6309ba29e
Keen-coffee <147879855+Keen-coffee@users.noreply.github.com>
2025-03-16 21:15:53 -0700

    Update __init__.py

--- components/mornsun_psu/__init__.py
+++ components/mornsun_psu/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c
-from esphome.const import CONF_ID, CONF_I2C_ADDRESS
+from esphome.const import CONF_ID, CONF_ADDRESS
 
 DEPENDENCIES = ['i2c']
 
@@ -12,12 +12,12 @@ MornsunPowerSupply = mornsun_psu_ns.class_('MornsunPowerSupply', cg.PollingCompo
 # Configuration schema
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(MornsunPowerSupply),
-    cv.Optional(CONF_I2C_ADDRESS, default=0x00): cv.i2c_address,
+    cv.Optional(CONF_ADDRESS, default=0x00): cv.i2c_address,
 }).extend(cv.polling_component_schema('10s')).extend(i2c.i2c_device_schema(0x00))
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
-    if CONF_I2C_ADDRESS in config:
-        cg.add(var.set_i2c_address(config[CONF_I2C_ADDRESS]))
+    if CONF_ADDRESS in config:
+        cg.add(var.set_i2c_address(config[CONF_ADDRESS]))
